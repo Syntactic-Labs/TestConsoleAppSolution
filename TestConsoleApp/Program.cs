0b6b5077e9bafd850a85fb52deb10926c044a17a
Michael Walker <walkerrepair255@yahoo.com>
2021-09-24 09:57:31 -0400

    Add project files.

---
+++ TestConsoleApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace TestConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Pancakes!!!!!");
+            var CostOfBootcamp = 15500.0;
+            var NumberOfDays = 65;
+            var CostPerDay = CostOfBootcamp / NumberOfDays;
+            Console.WriteLine($"I am paying ${CostPerDay} per day for boot camp!");
+        }
+    }
+}
